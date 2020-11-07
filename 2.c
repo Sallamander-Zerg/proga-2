ce522ff559c4815c729bc1224f76dae8814f60c6
SALL <62800760+Sallamander-Zerg@users.noreply.github.com>
2020-11-07 11:49:55 +0200

    Update 2.c

--- 2.c
+++ 2.c
@@ -3,42 +3,42 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/mman.h>
-
-
+#include <unistd.h>
 
 int main()
 {
+
 int a;
 int lm=0;
 int nm=0;
 int line[100][100];
 char ch;
-void *mmap(void *addr, size_t len, int prot, int flag, int filedes, off_t off);
-int fd = open("text.txt", O_RDONLY, 0);
-if(fd < 0) {
-  std::cerr << "fileMappingCreate - open failed, fname =  "
-            << fname << ", " << strerror(errno) << std::endl;
-  return nullptr;
-}
 struct FileMapping {
-  HANDLE hFile;
-  HANDLE hMapping;
+  int fd;
   size_t fsize;
   unsigned char* dataPtr;
 };
-while (read(file, &ch , sizeof(char))>0){
+int fd = open( "text.txt", O_RDONLY, 0 );
+if(fd<=0){
+printf("-1");
+}
+struct stat st;
+size_t fsize = (size_t)st.st_size;
+unsigned char* mass = (unsigned char*)mmap(0, fsize,PROT_READ,MAP_PRIVATE, fd, 0);
+line[0][0]=0;
+while (mass>0){
 nm++;
 if (ch == ('\n')) {
 line[lm][0]=nm;
 line[lm-1][1]=nm-line[lm-1][0];
 lm ++;
+printf("%d, %d, index = %d\n", line[lm-1][0],  line[lm-1][1], lm -1 );
 }
-
 }
 scanf("%d", &a);
-fseek(file,line[a][0],SEEK_SET);
-for(int i=0;i<line[a][1];i++){
-read(file, &ch , sizeof(char));
+//lseek(file,line[a-2][0],SEEK_SET);
+for(int i=0;i<line[a-2][1];i++){
+//read(file, &ch , sizeof(char));
 printf("%c",ch);
 }
 return 0;
